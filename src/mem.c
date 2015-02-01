dd3c8a11c85199c3237e80a2b67d8fa2b20d59e7
Amadou Sall <ahasall@yahoo.fr>
2015-02-01 02:17:42 +0100

    mem_alloc finalisee + debut de mem_free
    Signed-off-by: Amadou Sall <ahasall@yahoo.fr>

--- src/mem.c
+++ src/mem.c
@@ -9,7 +9,7 @@
 #include <assert.h>
 #include <math.h>
 #include "mem.h"
-
+#include <string.h>
 /**
    Structures de données
 **/
@@ -20,7 +20,7 @@ typedef struct ZL {
 
 ZL* TZL[BUDDY_MAX_INDEX + 1];
 
-void* zone_memoire = 0;
+void* zone_memoire = NULL;
 
 /**
    Fonctions de l'allocateur mémoire
@@ -29,6 +29,7 @@ void* zone_memoire = 0;
 int 
 mem_init()
 {
+  //  printf("init : %p \n", zone_memoire);
   if (! zone_memoire)
     zone_memoire = (void *) malloc(ALLOC_MEM_SIZE);
   if (zone_memoire == 0)
@@ -36,7 +37,6 @@ mem_init()
       perror("mem_init:");
       return -1;
     }
-
   // Etat initial, il n'y a qu'un bloc de taille maximale
   // Création de la zone mémoire principale
   ZL* zone_init = zone_memoire;
@@ -46,8 +46,9 @@ mem_init()
   for(int i = 0; i < BUDDY_MAX_INDEX; i++){
     TZL[i] = NULL;
   }
+  memset(zone_memoire,0,ALLOC_MEM_SIZE);
   TZL[BUDDY_MAX_INDEX] = zone_init;
-
+  
   
   return 0;
 }
@@ -55,34 +56,36 @@ mem_init()
 void *
 mem_alloc(unsigned long size)
 {
+  // printf("alloc : %p\n", zone_memoire);
   
-  if(size ==0) {
+  if(!zone_memoire || size <= 0 || size > ALLOC_MEM_SIZE) {
     return (void*)0;
   }
-  if(size == 1){
-    return (void*)0;
+  // Faut pouvoir stocker au moins un pointeur vers la prochaine zone libre lorsqu'on libéra cette zone
+  if(size < sizeof(unsigned long)){
+    size = sizeof(void*);
   }
 
   // On veut allouer un bloc de taille 2^k
   int k = ceil(log(size)/log(2));
   // Trouver un bloc de taille convenable
   int j = k;
-  if(j > BUDDY_MAX_INDEX){
-    printf("Pas de mémoire disponible");
-    return 0;
+  /* if(j > BUDDY_MAX_INDEX){ */
+  /*   printf("Pas de mémoire disponible"); */
+  /*   return 0; */
   
-      }else{
-    while (TZL[j] == NULL ) {
-      if(j > BUDDY_MAX_INDEX){
-	printf("Pas de mémoire disponible");
-	return 0; 
-      }
+  /*     }else{ */
+    while (TZL[j] == NULL && j <= BUDDY_MAX_INDEX) {
       j++;
     }
-  }
-  
-  void *adress = TZL[j];
-  TZL[j] = TZL[j]->suivant;
+      if(j > BUDDY_MAX_INDEX){
+	printf("Pas de mémoire disponible\n");
+	return (void*)0; 
+      }
+    
+// J'enlève le bloc de la liste
+    void *adress = TZL[j];
+    TZL[j] = TZL[j]->suivant;
   
   // Si on a pas besoin diviser le bloc trouvé alors c'est cool !
  split: if(j == k){
@@ -90,7 +93,7 @@ mem_alloc(unsigned long size)
   }
   // Split
   j--;
-  ZL *adress_buddy = (int)adress ^ (int)pow(2,j);
+  void *adress_buddy =(void*) ((unsigned long)adress ^ (unsigned long)pow(2,j));
   TZL[j] = adress_buddy;
   goto split;
   
@@ -101,6 +104,33 @@ int
 mem_free(void *ptr, unsigned long size)
 {
   /* ecrire votre code ici */
+  if(size < sizeof(unsigned long)){
+    size = sizeof(void*);
+  }
+  int k = ceil(log(size)/log(2));
+  // On teste si le compagnon est disponible
+  void *adress_buddy =(void*) ((unsigned long)ptr ^ (unsigned long)pow(2,k));
+  int buddy_is_available = 0;
+  ZL *courant = TZL[k];
+  while (courant != NULL) {
+    if(courant == adress_buddy){
+      buddy_is_available = 1;
+      break;
+    }
+    courant = courant->suivant;
+  }
+ 
+  if(!buddy_is_available){
+    //On met le block d'adresse ptr dans la liste d'ordre k dans le cas où le body n'est pas trouvé
+    ZL *zl = TZL[k];
+    TZL[k] = ptr;
+    TZL[k]->suivant = zl;
+  }else{
+    // Enlever le buddy de la liste indexé par TZL[k] puis ajouter ptr dans TZL[k+1]
+    mem_free(ptr,size+1);
+  }
+ 
+
 
   return 0;
 }
